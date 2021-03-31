439cf2ec2f1e55b4a0b7d6d0a3f96cb802af7da7
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-03-30 20:44:00 -0700

    Updated Menu component
    Removed hard-coded data import from MenuData.js. Instead, fetches menu data from our API

--- client/src/components/pages/Menu/index.js
+++ client/src/components/pages/Menu/index.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "../../Modal/";
 import MenuItem from "./MenuItem/";
-import { MenuData } from "../../Data";
 import "./style.css";
 
 const Menu = ({ smallDisplay, addToCart }) => {
+  const [Menu, setMenu] = useState([]);
   const [showModal, setshowModal] = useState(false);
+
+  useEffect(() => {
+    async function getMenu() {
+      try {
+        const response = await fetch("/api/drinks");
+        const data = await response.json();
+        setMenu(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getMenu();
+  }, []);
 
   const toggleModal = () => {
     setshowModal(!showModal);
@@ -20,7 +33,7 @@ const Menu = ({ smallDisplay, addToCart }) => {
   return (
     <main id="menu">
       <div className={smallDisplay ? "flex-col center-items" : "flex"}>
-        {MenuData.map((item) => {
+        {Menu.map((item) => {
           return (
             <MenuItem
               smallDisplay={smallDisplay}
