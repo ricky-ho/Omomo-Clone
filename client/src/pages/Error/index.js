184682e124418a6e1392aae98f6e6982ac65aaae
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-04-22 16:12:50 -0700

    Added 404 error route

---
+++ client/src/pages/Error/index.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+const ErrorPage = () => {
+  return (
+    <main className="page-not-found">
+      <img
+        src={`${process.env.PUBLIC_URL}/page-not-found.svg`}
+        alt="Page not found"
+        className="not-found-img"
+      />
+      <h1>Page not found</h1>
+      <p>The page you are looking for does not exist or has been moved.</p>
+      <Link to="/" className="link link-black">
+        GO BACK HOME
+      </Link>
+    </main>
+  );
+};
+
+export default ErrorPage;
