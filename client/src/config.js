94ca80f8b72f9357106279167455e8e7101817c4
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-09-24 20:35:06 -0700

    Deleted models folder
    Placed contents in config.js file

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,15 @@
-const storeLocations = [
+export const navlinks = [
+  {
+    title: "MENU",
+    path: "/menu",
+  },
+  {
+    title: "LOCATIONS",
+    path: "/locations",
+  },
+];
+
+export const storeLocations = [
   {
     title: "CHINO HILLS",
     address: "2983 Chino Ave #A8, Chino Hills CA 91709",
@@ -67,4 +78,59 @@ const storeLocations = [
   },
 ];
 
-export default storeLocations;
+/*
+const default_options = [
+  {
+    name: "ice",
+    limit: 1,
+    label: "Ice level / Hot",
+    opts: [
+      { label: "Regular ice", price: 0, selected: true },
+      { label: "Less ice", price: 0, selected: false },
+      { label: "No ice", price: 0, selected: false },
+      { label: "Extra ice", price: 0, selected: false },
+      { label: "Hot", price: 0, selected: false },
+    ],
+  },
+  {
+    name: "sweetness",
+    limit: 1,
+    label: "Sugar level",
+    opts: [
+      { label: "100%", price: 0, selected: true },
+      { label: "80%", price: 0, selected: false },
+      { label: "50%", price: 0, selected: false },
+      { label: "30%", price: 0, selected: false },
+      { label: "0%", price: 0, selected: false },
+      { label: "Extra sweet", price: 0, selected: false },
+    ],
+  },
+  {
+    name: "dairy",
+    limit: 1,
+    label: "Dairy alternatives",
+    opts: [
+      { label: "House milk", price: 0, selected: true },
+      { label: "Fresh milk", price: 0, selected: false },
+      { label: "Almond milk", price: 0.5, selected: false },
+      { label: "Oat milk", price: 0.5, selected: false },
+    ],
+  },
+  {
+    name: "toppings",
+    limit: null,
+    label: "Toppings",
+    opts: [
+      { label: "Honey boba", price: 0.5, selected: false },
+      { label: "Agar boba", price: 0.5, selected: false },
+      { label: "Aloe Vera", price: 0.5, selected: false },
+      { label: "Red bean", price: 0.5, selected: false },
+      { label: "Lychee jelly", price: 0.5, selected: false },
+      { label: "Coffee jelly", price: 0.75, selected: false },
+    ],
+  },
+];
+
+export default default_options;
+
+*/
