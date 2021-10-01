106f27bc3052fe3871fb9c5a0d83093a6aa946cd
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-09-30 20:17:53 -0700

    Update ErrorPage styles

--- client/src/pages/ErrorPage/index.js
+++ client/src/pages/ErrorPage/index.js
@@ -4,17 +4,14 @@ import "./style.scss";
 
 const ErrorPage = () => {
   return (
-    <main className="page-not-found">
+    <main id="page-not-found">
       <img
         src={`${process.env.PUBLIC_URL}/page-not-found.svg`}
         alt="Page not found"
-        className="not-found-img"
       />
       <h1>Page not found</h1>
       <p>The page you are looking for does not exist or has been moved.</p>
-      <Link to="/" className="link link-black">
-        GO BACK HOME
-      </Link>
+      <Link to="/">BACK HOME</Link>
     </main>
   );
 };
