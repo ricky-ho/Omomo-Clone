df83afbaceac1a84efade32fb19d95442863cb9a
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-10-03 18:54:01 -0700

    Add error component
    - Created Error component to display message to users
    - Handles failed async server requests to fetch for menu data

---
+++ client/src/components/Error/index.js
@@ -0,0 +1,12 @@
+import "./style.scss";
+
+const ErrorMessage = () => {
+  return (
+    <div className="fetch-error">
+      <h2>SERVER ERROR</h2>
+      <p>Please try again at a later time</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
--- client/src/utils/menuUtils.js
+++ client/src/utils/menuUtils.js
@@ -1,11 +1,13 @@
 export const getMenu = async () => {
   try {
     const response = await fetch(`/api/products`);
+
+    if (!response.ok) {
+      return Promise.reject(response);
+    }
+
     return response.json();
-  } catch (err) {
-    return {
-      error: err,
-      message: "Error: could not fetch products",
-    };
+  } catch (error) {
+    return Promise.reject(error);
   }
 };
