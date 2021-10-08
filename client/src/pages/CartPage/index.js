9d0b21d9be7bf7c0e96a7c5f11483703b689f199
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-10-08 13:52:11 -0700

    Modify CartPage
    Extracted parts of CartPage to create sub-components and added responsive typography.

--- client/src/pages/CartPage/index.js
+++ client/src/pages/CartPage/index.js
@@ -17,40 +17,49 @@ const CartPage = () => {
       <div className="cart__wrapper">
         <h1>SHOPPING CART</h1>
         <div className="cart__inner">
-          <section className="cart__list">
-            <div className="cart__items">
-              {cart.length > 0 ? (
-                cart.map((cartItem, index) => {
-                  return <Card key={index} item={cartItem} />;
-                })
-              ) : (
-                <div className="cart__empty">
-                  <h2>Your cart is currently empty</h2>
-                </div>
-              )}
-            </div>
-          </section>
-
-          <section className="cart__summary">
-            <h2>ORDER SUMMARY</h2>
-            <div>
-              <p>Total Items</p>
-              <p>{cart.length}</p>
-            </div>
-            <div>
-              <p>Subtotal</p>
-              <p>$10.00</p>
-            </div>
-            <form action="" onSubmit={(e) => e.preventDefault()}>
-              <button type="submit" disabled={cart.length === 0}>
-                Checkout
-              </button>
-            </form>
-          </section>
+          <CartList {...{ cart }} />
+          <CartSummary {...{ cart }} />
         </div>
       </div>
     </main>
   );
+};
+
+const CartList = ({ cart }) => {
+  return (
+    <section className="cart__list">
+      {cart.length > 0 ? (
+        <div className="cart__items">
+          {cart.map((cartItem, index) => {
+            return <Card key={index} item={cartItem} />;
+          })}
+        </div>
+      ) : (
+        <div className="cart__empty">
+          <h2>Your cart is currently empty</h2>
+        </div>
+      )}
+    </section>
+  );
+};
+
+const CartSummary = ({ cart }) => {
+  return cart.length > 0 ? (
+    <section className="cart__summary">
+      <h2>ORDER SUMMARY</h2>
+      <div>
+        <p>Total Items</p>
+        <p>{cart.length}</p>
+      </div>
+      <div>
+        <p>Subtotal</p>
+        <p>$10.00</p>
+      </div>
+      <form action="" onSubmit={(e) => e.preventDefault()}>
+        <button type="submit">Checkout</button>
+      </form>
+    </section>
+  ) : null;
 };
 
 export default CartPage;
