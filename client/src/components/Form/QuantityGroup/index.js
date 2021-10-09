a725eb12056f6e08275edb58adc0dfc0f5d83fda
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-10-09 16:37:06 -0700

    Update QuantityGroup
    Extracted the functions for the buttons' onClick handler rather than defining it inline

--- client/src/components/Form/QuantityGroup/index.js
+++ client/src/components/Form/QuantityGroup/index.js
@@ -5,28 +5,25 @@ import {
 import "./style.scss";
 
 const QuantityGroup = ({ quantity, dispatch }) => {
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_QUANTITY,
+    });
+  };
+
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT_QUANTITY,
+    });
+  };
+
   return (
     <fieldset name="quantity">
-      <button
-        type="button"
-        onClick={() =>
-          dispatch({
-            type: DECREMENT_QUANTITY,
-          })
-        }
-        disabled={quantity <= 1}
-      >
+      <button type="button" onClick={decrement} disabled={quantity <= 1}>
         -
       </button>
       <input type="number" value={quantity} readOnly />
-      <button
-        type="button"
-        onClick={() =>
-          dispatch({
-            type: INCREMENT_QUANTITY,
-          })
-        }
-      >
+      <button type="button" onClick={increment}>
         +
       </button>
     </fieldset>
