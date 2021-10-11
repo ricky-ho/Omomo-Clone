f11e1cba5c0f0225a2fb57d79beeef1f7d57e4f4
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-10-10 17:27:12 -0700

    Update CartPage and Card content and styles
    - Also reduced max width of the App layout to 1280px

---
+++ client/src/components/Card/CardContent/index.js
@@ -0,0 +1,41 @@
+import "./style.scss";
+
+const CardContent = ({ item }) => {
+  const { product, quantity } = item;
+
+  return (
+    <div className="details__content">
+      <div>
+        <input type="number" value={quantity} />
+        <div className="price">
+          <span>Price:</span>
+          <span>{`$${product.price.toFixed(2)}`}</span>
+        </div>
+      </div>
+
+      <ul>
+        {Object.keys(item.modifications).map((group, index) => {
+          const selection = item.modifications[group];
+
+          if (Array.isArray(selection)) {
+            return selection.map((opt, index) => {
+              return (
+                <li key={index} data-group={group}>
+                  <span>{opt.label}</span>
+                </li>
+              );
+            });
+          }
+
+          return (
+            <li key={index} data-group={group}>
+              <span>{selection.label}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default CardContent;
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,10 +1,42 @@
 import { useContext } from "react";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+
 import CartContext from "../../contexts/cart-context";
+import CardContent from "./CardContent";
 import "./style.scss";
 
 const Card = ({ index, item }) => {
   const { removeFromCart } = useContext(CartContext);
-  const { product, quantity } = item;
+  const { product } = item;
+
+  const CardHeader = () => {
+    return (
+      <div className="details__header">
+        <h3>{product.name}</h3>
+        <div>
+          <button type="button" className="item__edit" title={product.name}>
+            Edit Item
+          </button>
+          <button
+            type="button"
+            className="item__remove"
+            onClick={() => removeFromCart(index)}
+          >
+            <AiOutlineCloseSquare size={30} />
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  const CardFooter = () => {
+    return (
+      <div className="details__footer">
+        <span>Subtotal:</span>
+        <span>{`$${product.price.toFixed(2)}`}</span>
+      </div>
+    );
+  };
 
   return (
     <div className="card">
@@ -12,23 +44,9 @@ const Card = ({ index, item }) => {
         <img src={product.imageURL} alt={product.name} />
       </div>
       <div className="card__details">
-        <div className="details__header">
-          <h3>{product.name}</h3>
-          <div>
-            <button type="button">Edit</button>
-            <button type="button" onClick={() => removeFromCart(index)}>
-              Remove
-            </button>
-          </div>
-        </div>
-
-        <div className="details__content">
-          <div>{quantity}</div>
-          <div>
-            <p>Price:</p>
-            <p>{product.price}</p>
-          </div>
-        </div>
+        <CardHeader />
+        <CardContent {...{ item }} />
+        <CardFooter />
       </div>
     </div>
   );
