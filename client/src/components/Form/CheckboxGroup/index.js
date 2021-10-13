e75af44b3e3e76691718a3d6b58b2242002f5ac7
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-10-12 21:45:20 -0700

    Update Form subcomponents
    - Removed OptionGroup subcomponent and replaced with SelectionGroup and CheckboxGroup

---
+++ client/src/components/Form/CheckboxGroup/index.js
@@ -0,0 +1,52 @@
+import { useState, useEffect, useContext } from "react";
+
+import { ItemContext } from "../../../contexts/ItemState";
+import {
+  getAllSelectedCheckboxes,
+  isCheckboxSelected,
+} from "../../../utils/formUtils";
+
+import "./style.scss";
+
+const CheckboxGroup = ({ group }) => {
+  const { setSelectedOptions } = useContext(ItemContext);
+
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
+
+  useEffect(() => {
+    setSelectedOptions(group.label, selectedCheckboxes);
+  }, [selectedCheckboxes]);
+
+  const Checkbox = ({ index, option }) => {
+    const handleChange = () => {
+      const selected = getAllSelectedCheckboxes(selectedCheckboxes, option);
+      setSelectedCheckboxes(selected);
+    };
+
+    return (
+      <div key={index} className="checkbox__option">
+        <input
+          type="checkbox"
+          id={option._id}
+          onChange={handleChange}
+          checked={isCheckboxSelected(selectedCheckboxes, option)}
+        />
+        <div>
+          <label htmlFor={option._id}>{option.label}</label>
+          {option.price > 0 && <span>{`+${option.price.toFixed(2)}`}</span>}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <fieldset name={group.label} className="checkbox__fieldset">
+      <legend>{group.label}</legend>
+      {group.options.map((option, index) => {
+        return <Checkbox key={index} {...{ index, option }} />;
+      })}
+    </fieldset>
+  );
+};
+
+export default CheckboxGroup;
--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,9 +1,10 @@
-import { useContext, useReducer } from "react";
+import { useContext } from "react";
 
-import CartContext from "../../contexts/cart-context";
-import formReducer from "../../contexts/reducers/form-reducer";
-import OptionGroup from "./OptionGroup";
+import { CartContext } from "../../contexts/CartState";
+import { ItemContext } from "../../contexts/ItemState";
 import QuantityGroup from "./QuantityGroup";
+import SelectionGroup from "./SelectionGroup";
+import CheckboxGroup from "./CheckboxGroup";
 
 import "./style.scss";
 
@@ -12,27 +13,17 @@ import "./style.scss";
   - Add indicator that item has been added to cart
 */
 
-const Form = ({ toggleModal, options, product }) => {
+const Form = ({ toggleModal, product }) => {
   const { addToCart } = useContext(CartContext);
-  const defaultOptions = {
-    [options[0].label]: options[0].options[0],
-    [options[1].label]: options[1].options[0],
-    [options[2].label]: options[2].options[0],
-    [options[3].label]: [],
-  };
-  const initialState = {
-    selectedOptions: defaultOptions,
-    quantity: 1,
-  };
-
-  const [state, dispatch] = useReducer(formReducer, initialState);
+  const { quantity, options, selectedOptions } = useContext(ItemContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const newCartItem = {
       product: product,
-      quantity: state.quantity,
-      modifications: state.selectedOptions,
+      quantity: quantity,
+      modifications: selectedOptions,
     };
 
     addToCart(newCartItem);
@@ -41,11 +32,15 @@ const Form = ({ toggleModal, options, product }) => {
 
   return (
     <form className="modal__form" onSubmit={(e) => handleSubmit(e)}>
-      <QuantityGroup quantity={state.quantity} dispatch={dispatch} />
+      <QuantityGroup quantity={quantity} />
 
       {options &&
         options.map((group, index) => {
-          return <OptionGroup key={index} {...{ group, dispatch }} />;
+          if (group.limit === -1) {
+            return <CheckboxGroup key={index} {...{ group }} />;
+          }
+
+          return <SelectionGroup key={index} {...{ group }} />;
         })}
 
       <div>
