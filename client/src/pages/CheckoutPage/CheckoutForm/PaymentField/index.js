d760273e80246a79f9cd12f5968a7f74ce13e56a
Ricky Ho <36563085+ricky-ho@users.noreply.github.com>
2021-10-24 15:04:42 -0700

    Update CheckoutForm PaymentField
    - Added dropdown to display demo payment information
    - Added styling to payment error messages

--- client/src/pages/CheckoutPage/CheckoutForm/PaymentField/index.js
+++ client/src/pages/CheckoutPage/CheckoutForm/PaymentField/index.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { CardElement } from "@stripe/react-stripe-js";
 import { FaLock } from "react-icons/fa";
+import { BiChevronsDown, BiChevronsUp, BiCheckShield } from "react-icons/bi";
+import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
+
+import "./style.scss";
 
 const PaymentField = () => {
-  /* TODO: Check that payment field is non-empty before submitting? */
   return (
     <fieldset className="checkout-form__payment-field">
       <legend>
@@ -24,14 +28,13 @@ const PaymentField = () => {
         </p>
       </legend>
 
-      <div className="form-row" id="card-number">
+      <div className="form-row" id="card-element">
         <CardElement
-          id="card-element"
           options={{
             style: {
               base: {
                 fontSize: "16px",
-                color: "gray",
+                color: "black",
               },
               invalid: {
                 color: "red",
@@ -41,7 +44,66 @@ const PaymentField = () => {
           }}
         />
       </div>
+      <DemoPaymentInfo />
     </fieldset>
   );
 };
+
+const DemoPaymentInfo = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="payment-demo">
+      <div className="demo-info-container">
+        <button
+          type="button"
+          className="demo-btn"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span>Demo - Payment Information</span>
+          <span>
+            {isOpen ? <BiChevronsUp size={16} /> : <BiChevronsDown size={16} />}
+          </span>
+        </button>
+        <div className={`demo-content ${isOpen ? "demo-expand" : ""}`}>
+          <div className="demo-instruction">
+            For demonstration purposes, the following test card numbers have
+            been provided. Enter any valid expiration date in the future, any
+            random CVC number, and any zip code.
+          </div>
+          <ul>
+            <li data-demo-card="success">
+              <div className="demo-item-icon">
+                <AiOutlineCheckCircle size={16} />
+              </div>
+              <div className="demo-item-info">
+                <span>Successful Payment:</span>
+                <span>4242 4242 4242 4242</span>
+              </div>
+            </li>
+            <li data-demo-card="fail">
+              <div className="demo-item-icon">
+                <AiOutlineCloseCircle size={16} />
+              </div>
+              <div className="demo-item-info">
+                <span>Unsuccessful Payment:</span>
+                <span>4000 0000 0000 0101</span>
+              </div>
+            </li>
+            <li data-demo-card="3ds">
+              <div className="demo-item-icon">
+                <BiCheckShield size={16} />
+              </div>
+              <div className="demo-item-info">
+                <span>3DS Payment:</span>
+                <span>4000 0027 6000 3184</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default PaymentField;
